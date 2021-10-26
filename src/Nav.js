10917989f47a25786dd63b69587519d39bb371b5
REIMI SASAKI <rey.my013@gmail.com>
2021-10-25 17:30:20 -0700

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scroll > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavBar);
+    return () => window.removeEventListener('scroll', transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+        <img className="nav__avatar" src="Netflix-avatar.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBjQvnVEcqNuQAEaRaSGEgaLvOheNxdK-0',
+  authDomain: 'netflix-clone-b2ac6.firebaseapp.com',
+  projectId: 'netflix-clone-b2ac6',
+  storageBucket: 'netflix-clone-b2ac6.appspot.com',
+  messagingSenderId: '857588832975',
+  appId: '1:857588832975:web:c76aa4e8b4606e890435ba',
+  measurementId: 'G-DN84REK8XW',
+};
